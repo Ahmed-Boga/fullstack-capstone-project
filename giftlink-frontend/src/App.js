48b499615ad8d357182da4ecc82169630ddb894f
Ahmed-Boga <146300379+Ahmed-Boga@users.noreply.github.com>
2024-10-31 06:50:03 +0200

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import Profile from './components/Profile/Profile';
+import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 function App() {
